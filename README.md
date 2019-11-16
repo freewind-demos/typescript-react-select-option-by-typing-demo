0e433cf29ddf50e00190d052b77c724913669b78
Freewind <nowindlee@gmail.com>
2019-11-17 00:56:16 +0800

    typescript-react-select-option-by-typing-demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React Select Option By Typing Demo
+=============================================
+
+在纯html中，一个multiple的select，如果需要给它设置selected values的话，不能使用`value`属性，而是要在option中设置`selected`。
+
+但是在react中，对select进行了增强，可以把一个array设置给value，从而直接支持设置selected values
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,37 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useState} from 'react'
+
+type Option = {
+  value: string,
+  label: string
+}
+
+const options: Option[] = [
+  {value: '111', label: '111'},
+  {value: '222', label: '222'},
+  {value: '333', label: '223'}
+]
 
 export default function Hello() {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+  function searchKeyword(keyword: string) {
+    const found = options.filter(it => it.label.startsWith(keyword));
+    setSelectedValues(found.map(it => it.value));
+  }
+
   return <div>
-    <h1>Hello React</h1>
+    <h1>Select Options by Typing</h1>
+    <div>
+      <div><input type="text" onChange={event => searchKeyword(event.target.value)}/></div>
+      <div>
+        <select multiple value={selectedValues}>
+          {
+            options.map(({value, label}) =>
+              <option key={value} value={value}>{label}</option>)
+          }
+        </select>
+      </div>
+    </div>
   </div>
 };
